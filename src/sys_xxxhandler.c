c7cc658787edaf453281d5340bd2a29c13dc70e4
khoibkpy <khoi.doanbkpy@hcmut.edu.vn>
2025-04-10 18:36:53 +0700

    Add syscall 440

---
+++ src/sys_xxxhandler.c
@@ -0,0 +1,10 @@
+#include "common.h"
+#include "syscall.h"
+#include "stdio.h"
+
+int __sys_xxxhandler(struct pcb_t *caller, struct sc_regs* regs)
+{
+    /* data */
+    printf("The first system call parameter %d\n", regs->a1);
+    return 0;
+};
