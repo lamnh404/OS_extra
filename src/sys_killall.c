c1927b822abeb06cc0d437d24bf15bc8add5f0f6
khoibkpy <khoi.doanbkpy@hcmut.edu.vn>
2025-04-22 10:28:12 +0700

    syscall final

--- src/sys_killall.c
+++ src/sys_killall.c
@@ -23,13 +23,11 @@ int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
 
     //hardcode for demo only
     uint32_t memrg = regs->a3;
-    /* TODO: Get name of the target proc */
+    /* Get name of the target proc */
     int i = 0;
     data = 0;
     while(data != -1){
-        // int tmp = i;
         libread(caller, memrg, i, &data);
-        // i = tmp; //how and why
         proc_name[i] = data;
         if(data == -1) proc_name[i] = '\0';
         i++;
@@ -50,8 +48,8 @@ int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
             }
             temp_name[j] = '\0';
             if (q->proc[read_idx] != NULL && strcmp(temp_name, proc_name) == 0) {
-                // free_pcb_memph(q->proc[read_idx]); //nothing happen
                 q->proc[read_idx]->pc = q->proc[read_idx]->code->size;
+                free_pcb_memph(q->proc[read_idx]);
                 q->proc[read_idx] = NULL;
             } else {
                 if (read_idx != write_idx) {
@@ -77,8 +75,8 @@ int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
             }
             temp_name[j] = '\0';
             if (q->proc[read_idx] != NULL && strcmp(temp_name, proc_name) == 0) {
-                // free_pcb_memph(q->proc[read_idx]); (not working)
-                free(q->proc[read_idx]); //khong biet co free duoc khong, chua test
+                q->proc[read_idx]->pc = q->proc[read_idx]->code->size;
+                free_pcb_memph(q->proc[read_idx]);
                 q->proc[read_idx] = NULL;
             } else {
                 if (read_idx != write_idx) {
