1a2456fc1abe20862224f4d5af2571ec89d77ce8
thi2351 <thi.nguyenmanhfw@gmail.com>
2025-04-26 14:23:20 +0000

    Fix schd and rbtree

--- src/sched.c
+++ src/sched.c
@@ -4,175 +4,146 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+#ifdef CFS_SCHED
+#include "cfs.h"
+#endif
+
 static struct queue_t ready_queue;
 static struct queue_t run_queue;
 static pthread_mutex_t queue_lock;
 
-static struct queue_t running_list;
 #ifdef MLQ_SCHED
 static struct queue_t mlq_ready_queue[MAX_PRIO];
 static int slot[MAX_PRIO];
 static int curr_prior = 0;
 #endif
 
-int queue_empty(void)
-{
-#ifdef MLQ_SCHED
-	unsigned long prio;
-	for (prio = 0; prio < MAX_PRIO; prio++)
-		if (!empty(&mlq_ready_queue[prio]))
-			return -1;
+#ifdef CFS_SCHED
+static pthread_mutex_t cfs_lock; // protect CFS runqueue
 #endif
-	return (empty(&ready_queue) && empty(&run_queue));
-}
 
-void init_scheduler(void)
-{
-#ifdef MLQ_SCHED
-	int i;
-
-	for (i = 0; i < MAX_PRIO; i++)
-	{
-		mlq_ready_queue[i].size = 0;
-		slot[i] = MAX_PRIO - i;
-	}
-#endif
-	ready_queue.size = 0;
-	run_queue.size = 0;
-	pthread_mutex_init(&queue_lock, NULL);
-}
-
-#ifdef MLQ_SCHED
-/*
- *  Stateful design for routine calling
- *  based on the priority and our MLQ policy
- *  We implement stateful here using transition technique
- *  State representation   prio = 0 .. MAX_PRIO, curr_slot = 0..(MAX_PRIO - prio)
- */
-struct pcb_t *get_mlq_proc(void)
-{
-	/*TODO: get a process from PRIORITY [ready_queue].
-	 * Remember to use lock to protect the queue.
-	 * */
-	pthread_mutex_lock(&queue_lock);
-	struct pcb_t *proc = NULL;
-	unsigned long prio;
-	bool isEmpty = false;
-	curr_prior = 0;
-	for (prio = curr_prior; prio < MAX_PRIO; prio++)
-	{
-		curr_prior = prio;
-		if (!empty(&mlq_ready_queue[prio]))
-		{
-			slot[prio] -= 1;
-			proc = dequeue(&mlq_ready_queue[prio]);
-			if (slot[prio] == 0 || empty(&mlq_ready_queue[prio]))
-			{
-				slot[prio] = MAX_PRIO - prio;
-				curr_prior++;
-				if (curr_prior >= MAX_PRIO)
-				{
-					curr_prior = 0;
-				}
-			}
-			break;
-		}
-		if (curr_prior >= MAX_PRIO)
-		{
-			curr_prior = 0;
-			if (isEmpty)
-				break;
-			isEmpty = true;
-			prio = -1;
-		}
-	}
-	pthread_mutex_unlock(&queue_lock);
-	return proc;
-}
-
-void put_mlq_proc(struct pcb_t *proc)
-{
-	pthread_mutex_lock(&queue_lock);
-	enqueue(&mlq_ready_queue[proc->prio], proc);
-	pthread_mutex_unlock(&queue_lock);
-}
-
-void add_mlq_proc(struct pcb_t *proc)
-{
-	pthread_mutex_lock(&queue_lock);
-	enqueue(&mlq_ready_queue[proc->prio], proc);
-	pthread_mutex_unlock(&queue_lock);
-}
-
-struct pcb_t *get_proc(void)
-{
-	return get_mlq_proc();
-}
-
-void put_proc(struct pcb_t *proc)
-{
-	proc->ready_queue = &ready_queue;
-	proc->mlq_ready_queue = mlq_ready_queue;
-	proc->running_list = &running_list;
-
-	/* TODO: put running proc to running_list */
-	put_mlq_proc(proc);
-	pthread_mutex_lock(&queue_lock);
-	dequeue(&run_queue);
-	enqueue(&run_queue, proc);
-	pthread_mutex_unlock(&queue_lock);
-	proc->running_list = &run_queue; //temp
-}
-
-void add_proc(struct pcb_t *proc)
-{
-	proc->ready_queue = &ready_queue;
-	proc->mlq_ready_queue = mlq_ready_queue;
-	proc->running_list = &running_list;
-
-	/* TODO: put running proc to running_list */
-
-	add_mlq_proc(proc);
-	pthread_mutex_lock(&queue_lock);
-	enqueue(&ready_queue, proc);
-	pthread_mutex_unlock(&queue_lock);
-	proc->running_list = &run_queue; //temp
-}
+// ===== Generic Helpers =====
+int queue_empty(void) {
+#ifdef CFS_SCHED
+    return (cfs_rq.total_weight == 0) ? 0 : -1;
+#elif defined(MLQ_SCHED)
+    for (int i = 0; i < MAX_PRIO; i++) {
+        if (!empty(&mlq_ready_queue[i])) return -1;
+    }
+    return (empty(&ready_queue) && empty(&run_queue));
 #else
-struct pcb_t *get_proc(void)
-{
-	struct pcb_t *proc = NULL;
-	/*TODO: get a process from [ready_queue].
-	 * Remember to use lock to protect the queue.
-	 * */
-	for (int i = 0; i < ready_queue.size; i++)
-	{
-		proc = dequeue(&ready_queue);
-	}
-	return proc;
+    return (empty(&ready_queue) && empty(&run_queue));
+#endif
 }
 
-void put_proc(struct pcb_t *proc)
-{
-	proc->ready_queue = &ready_queue;
-	proc->running_list = &running_list;
-
-	/* TODO: put running proc to running_list */
-
-	pthread_mutex_lock(&queue_lock);
-	enqueue(&run_queue, proc);
-	pthread_mutex_unlock(&queue_lock);
+void init_scheduler(void) {
+    pthread_mutex_init(&queue_lock, NULL);
+#ifdef CFS_SCHED
+    pthread_mutex_init(&cfs_lock, NULL);
+    cfs_init_rq();
+#elif defined(MLQ_SCHED)
+    for (int i = 0; i < MAX_PRIO; i++) {
+        mlq_ready_queue[i].size = 0;
+        slot[i] = MAX_PRIO - i;
+    }
+#else
+    ready_queue.size = 0;
+    run_queue.size = 0;
+#endif
 }
 
-void add_proc(struct pcb_t *proc)
-{
-	proc->ready_queue = &ready_queue;
-	proc->running_list = &running_list;
+// ===== Round-Robin Implementation =====
+static void rr_refill(void) {
+    while (!empty(&run_queue)) {
+        enqueue(&ready_queue, dequeue(&run_queue));
+    }
+}
 
-	/* TODO: put running proc to running_list */
+static struct pcb_t *rr_get(void) {
+    if (empty(&ready_queue) && !empty(&run_queue)) {
+        rr_refill();
+    }
+    return empty(&ready_queue) ? NULL : dequeue(&ready_queue);
+}
 
-	pthread_mutex_lock(&queue_lock);
-	enqueue(&ready_queue, proc);
-	pthread_mutex_unlock(&queue_lock);
+static void rr_add(struct pcb_t *proc) {
+    enqueue(&ready_queue, proc);
+}
+
+static void rr_put(struct pcb_t *proc) {
+    enqueue(&run_queue, proc);
+}
+
+// ===== MLQ Implementation =====
+#ifdef MLQ_SCHED
+static void add_mlq_proc(struct pcb_t *proc) {
+    enqueue(&mlq_ready_queue[proc->prio], proc);
+}
+
+static struct pcb_t *get_mlq_proc(void) {
+    struct pcb_t *proc = NULL;
+    for (int cnt = 0; cnt < MAX_PRIO; cnt++) {
+        int prio = (curr_prior + cnt) % MAX_PRIO;
+        if (!empty(&mlq_ready_queue[prio])) {
+            slot[prio]--;
+            proc = dequeue(&mlq_ready_queue[prio]);
+            if (slot[prio] == 0 || empty(&mlq_ready_queue[prio])) {
+                slot[prio] = MAX_PRIO - prio;
+                curr_prior = (prio + 1) % MAX_PRIO;
+            } else {
+                curr_prior = prio;
+            }
+            break;
+        }
+    }
+    return proc;
 }
 #endif
+
+// ===== Public Scheduler API =====
+struct pcb_t *get_proc(void) {
+    pthread_mutex_lock(&queue_lock);
+#ifdef CFS_SCHED
+    pthread_mutex_lock(&cfs_lock);
+    struct pcb_t *p = cfs_pick_next();
+    pthread_mutex_unlock(&cfs_lock);
+#elif defined(MLQ_SCHED)
+    struct pcb_t *p = mlq_get();
+    if (!p) p = rr_get();
+#else
+    struct pcb_t *p = rr_get();
+#endif
+    pthread_mutex_unlock(&queue_lock);
+    return p;
+}
+
+void add_proc(struct pcb_t *proc) {
+    pthread_mutex_lock(&queue_lock);
+#ifdef CFS_SCHED
+    proc->cfs_ent.vruntime = 0;
+    proc->cfs_ent.weight   = cfs_compute_weight(proc->cfs_ent.weight);
+    pthread_mutex_lock(&cfs_lock);
+    cfs_enqueue(proc);
+    pthread_mutex_unlock(&cfs_lock);
+#elif defined(MLQ_SCHED)
+    mlq_add(proc);
+#else
+    rr_add(proc);
+#endif
+    pthread_mutex_unlock(&queue_lock);
+}
+
+void put_proc(struct pcb_t *proc) {
+    pthread_mutex_lock(&queue_lock);
+#ifdef CFS_SCHED
+    pthread_mutex_lock(&cfs_lock);
+    cfs_enqueue(proc);
+    pthread_mutex_unlock(&cfs_lock);
+#elif defined(MLQ_SCHED)
+    mlq_add(proc);
+#else
+    rr_put(proc);
+#endif
+    pthread_mutex_unlock(&queue_lock);
+}
